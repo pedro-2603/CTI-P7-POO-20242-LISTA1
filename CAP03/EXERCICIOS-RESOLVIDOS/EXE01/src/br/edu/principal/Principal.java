b68815f291988f7c5aa67399213190449d686e59
pedro-2603 <161750403+pedro-2603@users.noreply.github.com>
2024-10-28 13:33:05 -0300

    Envio da ativida

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE01/src/br/edu/principal/Principal.java
@@ -0,0 +1,14 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		 double n1 = 7.5;
+	        double n2 = 8.5;
+	        double n3 = 6.0;
+	        double n4 = 5.0;
+	        double soma = n1 + n2 + n3 + n4;
+	        System.out.println(soma);
+	}
+
+}
