7697d2ae4dda24505ec553c3bb9cb84b3703d482
pedro-2603 <161750403+pedro-2603@users.noreply.github.com>
2024-10-28 14:31:40 -0300

    Envio da atividade 14

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE14/src/br/edu/principal/Principal.java
@@ -0,0 +1,11 @@
+package br.edu.principal;
+
+public class Principal {
+	public static void main(String[] args) {
+		int ano_nascimento = 2008;
+		int ano_atual = 2024;
+		System.out.println("A idade da pessoa é " + (ano_atual - ano_nascimento + " anos"));
+		System.out.println("Em 2050, a idade da pessoa será " + (2050 - ano_nascimento) + " anos");
+	}
+	
+}
