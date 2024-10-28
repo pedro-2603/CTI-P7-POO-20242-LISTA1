5ec8dcb38b94ce2325f1c90e4ea84ff83f348d85
pedro-2603 <161750403+pedro-2603@users.noreply.github.com>
2024-10-28 14:19:13 -0300

    Envio da atividade 12

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE12/src/br/edu/principal/Principal.java
@@ -0,0 +1,10 @@
+package br.edu.principal;
+
+public class Principal {
+    public static void main(String[] args){
+        double n1 = 2;
+        double n2 = 3;
+        System.out.println("2 elevado a 3 é " + Math.pow(n1,n2));
+        System.out.println("3 elevado a 2 é "+ Math.pow(n2,n1));
+    }
+}
