4cd93357573313a02ae53d3f88999ea57c863de5
pedro-2603 <161750403+pedro-2603@users.noreply.github.com>
2024-10-28 13:51:51 -0300

    Envio da atividade 04

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE04/src/br/edu/principal/Principal.java
@@ -0,0 +1,9 @@
+package br.edu.principal;
+
+public class Principal {
+    public static void main(String[] args){
+        double salario = 1000.0; 
+        double novo_salario = salario + (salario * 0.25);
+        System.out.println("O novo salário é " + novo_salario);
+        }
+    }
