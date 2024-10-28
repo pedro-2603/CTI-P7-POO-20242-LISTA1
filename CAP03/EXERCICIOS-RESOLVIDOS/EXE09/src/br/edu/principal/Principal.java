a212cc97a015eea5e0b6bc2a552eb1144aaddf41
pedro-2603 <161750403+pedro-2603@users.noreply.github.com>
2024-10-28 14:10:39 -0300

    Envio da atividade 09

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE09/src/br/edu/principal/Principal.java
@@ -0,0 +1,10 @@
+package br.edu.principal;
+
+public class Principal {
+    public static void main(String[] args){
+        double base = 20;
+        double altura = 10;
+        double area = (base * altura)/2;
+        System.out.print("A área do triángulo é " + area);
+        }
+    }
