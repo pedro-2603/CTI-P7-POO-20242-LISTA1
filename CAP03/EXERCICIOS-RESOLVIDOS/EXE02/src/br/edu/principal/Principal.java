47472ce63381b93d57c4ecfeafafe81d703fa867
pedro-2603 <161750403+pedro-2603@users.noreply.github.com>
2024-10-28 13:38:29 -0300

    Envio da atividade 02

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE02/src/br/edu/principal/Principal.java
@@ -0,0 +1,14 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		 double n1 = 7.5;
+	        double n2 = 8.5;
+	        double n3 = 6.0;
+	        double media = (n1 + n2 + n3)/3;
+	        System.out.println("A média das notas é " + media);
+
+	}
+
+}
