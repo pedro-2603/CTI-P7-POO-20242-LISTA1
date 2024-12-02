df935d9f295a4a2f9e6211c1d15fc20fc9eef7e0
pedro-2603 <161750403+pedro-2603@users.noreply.github.com>
2024-12-02 13:26:06 -0300

    Update Principal.java

--- CAP08/EXERCICÍOS-RESOLVIDOS/EXE03/src/br/edu/principal/Principal.java
+++ CAP08/EXERCICÍOS-RESOLVIDOS/EXE03/src/br/edu/principal/Principal.java
@@ -3,32 +3,33 @@ package br.edu.principal;
 import java.util.Scanner;
 
 public class Principal {
-
     public static void main(String[] args) {
-        // Criando o objeto Scanner para capturar as entradas do usuário
         Scanner scanner = new Scanner(System.in);
+        int a, b, c, result;
 
-        // Declaração das variáveis
-        int n1, n2;
+        do {
+            System.out.print("Digite um valor para 'a' (maior que 1): ");
+            a = scanner.nextInt();
+        } while (a <= 1);
 
-        // Exibe a introdução
-        System.out.println("Descubra qual número é maior!\n");
+        System.out.print("Digite o valor de 'b': ");
+        b = scanner.nextInt();
 
-        // Entrada dos números
-        System.out.print("Digite um número: ");
-        n1 = scanner.nextInt();
+        System.out.print("Digite o valor de 'c': ");
+        c = scanner.nextInt();
 
-        System.out.print("Digite outro número: ");
-        n2 = scanner.nextInt();
+        result = divisores(a, b, c);
 
-        // Comparação dos números
-        if (n1 > n2) {
-            System.out.printf("%d é maior que %d\n", n1, n2);
-        } else {
-            System.out.printf("%d é maior que %d\n", n2, n1);
+        System.out.println("Soma dos inteiros = " + result);
+    }
+
+    public static int divisores(int a, int b, int c) {
+        int soma = 0;
+        for (int i = b; i <= c; i++) {
+            if (i % a == 0) {
+                soma += i;
+            }
         }
-
-        // Fechando o scanner após o uso
-        scanner.close();
+        return soma;
     }
 }
