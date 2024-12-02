afd52a7e5b8d0e0419d2eb149bb81adfd2a88ce9
pedro-2603 <161750403+pedro-2603@users.noreply.github.com>
2024-12-02 13:28:01 -0300

    Update Principal.java

--- CAP08/EXERCICÍOS-RESOLVIDOS/EXE04/src/br/edu/principal/Principal.java
+++ CAP08/EXERCICÍOS-RESOLVIDOS/EXE04/src/br/edu/principal/Principal.java
@@ -5,48 +5,20 @@ import java.util.Scanner;
 public class Principal {
 
     public static void main(String[] args) {
-        // Criando o objeto Scanner para capturar as entradas do usuário
         Scanner scanner = new Scanner(System.in);
 
-        // Declaração das variáveis
-        int n1, n2, n3;
+        System.out.print("Digite o número de segundos: ");
+        int seg = scanner.nextInt();
 
-        // Exibe a introdução
-        System.out.println("Números em ordem crescente\n");
-        System.out.println("Digite 3 números inteiros diferentes.\n");
+        transformacao(seg);
+    }
 
-        // Entrada dos números
-        System.out.print("Digite um número: ");
-        n1 = scanner.nextInt();
+    public static void transformacao(int segundos) {
+        int h = segundos / 3600;
+        int r = segundos % 3600;
+        int m = r / 60;
+        int s = r % 60;
 
-        System.out.print("Digite um número: ");
-        n2 = scanner.nextInt();
-
-        System.out.print("Digite um número: ");
-        n3 = scanner.nextInt();
-
-        // Exibe os números em ordem crescente
-        System.out.println("\nA ordem crescente dos números é:");
-
-        // Condições para determinar a ordem crescente dos números
-        if ((n1 > n2) && (n2 > n3)) {
-            System.out.printf("%d, %d, %d\n", n3, n2, n1);
-        } else if ((n1 > n3) && (n2 > n3)) {
-            System.out.printf("%d, %d, %d\n", n2, n3, n1);
-        } else if ((n2 > n1) && (n1 > n3)) {
-            System.out.printf("%d, %d, %d\n", n3, n1, n2);
-        } else if ((n2 > n3) && (n3 > n1)) {
-            System.out.printf("%d, %d, %d\n", n1, n3, n2);
-        } else if ((n3 > n1) && (n1 > n2)) {
-            System.out.printf("%d, %d, %d\n", n2, n1, n3);
-        } else if ((n3 > n2) && (n2 > n1)) {
-            System.out.printf("%d, %d, %d\n", n1, n2, n3);
-        } else {
-            // Se as condições acima não forem satisfeitas, significa que os números não são diferentes
-            System.out.println("Digite números diferentes\n");
-        }
-
-        // Fechando o scanner após o uso
-        scanner.close();
+        System.out.println("Horas: " + h + ", Minutos: " + m + ", Segundos: " + s);
     }
 }
