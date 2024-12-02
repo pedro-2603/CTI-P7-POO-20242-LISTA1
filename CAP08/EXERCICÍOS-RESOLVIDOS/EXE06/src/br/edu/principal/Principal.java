edbe8ce48061f80cc10aa82b0f5f92415a2aff54
pedro-2603 <161750403+pedro-2603@users.noreply.github.com>
2024-12-02 13:30:39 -0300

    Update Principal.java

--- CAP08/EXERCICÍOS-RESOLVIDOS/EXE06/src/br/edu/principal/Principal.java
+++ CAP08/EXERCICÍOS-RESOLVIDOS/EXE06/src/br/edu/principal/Principal.java
@@ -1,5 +1,27 @@
 package br.edu.principal;
+import java.util.Scanner;
 
 public class Principal {
+    public static void multiplicacao(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i * j + " ");
+            }
+            System.out.println();
+        }
+    }
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um número entre 1 e 9: ");
+        int num = scanner.nextInt();
+
+        while (num < 1 || num > 9) {
+            System.out.print("Número inválido! Digite um número entre 1 e 9: ");
+            num = scanner.nextInt();
+        }
+
+        multiplicacao(num);
+        scanner.close();
+    }
 }
